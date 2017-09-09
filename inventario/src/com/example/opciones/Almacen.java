a7a011f961b6d9735645942be0eb699f92e56e83
hbetancourt <hector@tuprograma.mx>
2017-09-08 20:38:46 -0500

    Ajuste de numberfield

--- inventario/src/com/example/opciones/Almacen.java
+++ inventario/src/com/example/opciones/Almacen.java
@@ -14,6 +14,7 @@ import java.util.TimeZone;
 
 import org.vaadin.dialogs.ConfirmDialog;
 import org.vaadin.haijian.ExcelExporter;
+//import org.vaadin.ui.NumberField;
 import org.vaadin.ui.NumberField;
 
 import sql.SqlConf;
